0bd9abfe14bb6316b7d1c436a9666c7e00cedd43
Aggressive-Mohammed <ibrahimmohammevis@gmail.com>
2024-04-03 04:34:22 -0700

    complete

---
+++ src/measures_of_central_tendency_app/Measures_Of_Central_Tendency_App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package measures_of_central_tendency_app;
+
+/**
+ *
+ * @author MOHAMMED
+ */
+public class Measures_Of_Central_Tendency_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Home_Page_Ibrahim_Mohammed main = new Home_Page_Ibrahim_Mohammed();
+     main.setVisible(true);
+       
+    }
+    
+}
